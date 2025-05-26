9ec221b9467e324b32af5e29b95a5e8b8c04abc3
SproutBoxGaming <elliotmargison@gmail.com>
2025-05-26 20:21:34 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
+
